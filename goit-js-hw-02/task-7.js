14862fcfb2647a49b28c4004a91ecf4212ab1497
Kukharenko Nedzhat <nexuak@gmail.com>
2020-05-27 15:09:16 +0300

    added7task

--- goit-js-hw-02/task-7.js
+++ goit-js-hw-02/task-7.js
@@ -1,4 +1,5 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+console.log(logins);
 
 const isLoginValid = function(login) {
   if (login.length >= 4 && login.length <= 16) {
@@ -10,7 +11,6 @@ const isLoginValid = function(login) {
 
 const isLoginUnique = function(allLogins, login) {
   let testLogin = logins.includes(login);
-
   if (testLogin === true){
       return false;
   } else {
@@ -19,21 +19,27 @@ const isLoginUnique = function(allLogins, login) {
 };
 
 const addLogin = function(allLogins, login) {
-  
-
-
-    
+  let firstCheckLogin = isLoginValid(login);
+  if(firstCheckLogin === false) {
+    return 'Ошибка! Логин должен быть от 4 до 16 символов';
+  } else {
+    let secondCheckLogin = isLoginUnique(allLogins, login);
+      if(secondCheckLogin === false) {
+        return 'Такой логин уже используется!';
+      } else {
+        logins.push(login);
+        return 'Логин успешно добавлен!';
+      }
+  }
 };
 
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+console.log(logins);
 
 
 
 
-
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
-// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
-// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
